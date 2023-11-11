6438a313dfb9b9be7010835e5bfedd6d1cc40d94
Guakocius <engelhardt.alexander@mein.gmx>
2023-11-11 14:30:59 +0100

    updated fitness center

---
+++ Portfolio/Fitness-center/booking.js
@@ -0,0 +1,24 @@
+const subscriptions = [
+    {
+        name: "Standard",
+        price: 49.99,
+        description: "Standard model for basic features"
+    },
+    {
+        name: "Premium",
+        price: 69.99,
+        description: "Premium model for all features"
+    }
+];
+
+function generateModelContent(model, modelID) {
+    const modelContainer = document.getElementById("modelID");
+
+    if(!modelContainer) {
+        console.error(`Container with ID ${modelID} not found.`);
+    }
+}
+
+function checkout() {
+
+}
